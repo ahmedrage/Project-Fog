c722329abc6141ff9796cf7f079c8e02a0873942
HIVAladeen <lachiecs@hotmail.com>
2015-11-28 05:49:21 +1030

    did fuck all
    cuz I am a swagfag

--- Project Fog/Assets/Scripts/player.cs
+++ Project Fog/Assets/Scripts/player.cs
@@ -5,33 +5,50 @@ public class player : MonoBehaviour {
 
 	public Rigidbody2D rb;
 	public float speed;
-	public float velocity;
-	public float rotate;
+	public Transform cursor;
+	public int direction;
 	// Use this for initialization
 	void Start () 
 	{
-		//Cursor.visible = false;
+		direction = 1;
+		Cursor.visible = false;
 		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		cursor.position = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 		float moveVertical = Input.GetAxisRaw ("Vertical");
 		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
 
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb.velocity = (movement * speed);
 
-		if(Input.mousePosition.x < transform.localScale.x)
+
+
+		if(cursor.position.x < transform.position.x && direction != 2)
 		{
-			transform.localScale = new Vector2(transform.localScale.x* -1, transform.localScale.y*1);
+			direction = 2;
+			horizontalFlip();
 		}
+
+		if (cursor.position.x > transform.position.x && direction != 1) 
+		{
+			direction = 1;
+			horizontalFlip();
+		}
+
 
 		if (Input.GetKeyDown (KeyCode.R)) 
 		{
 			Application.LoadLevel (Application.loadedLevel);
 		}
+	}
+
+	void horizontalFlip() {
+
+		transform.localScale = new Vector2(transform.localScale.x* -1, transform.localScale.y*1);
 	}
 }
 
---
+++ Project Fog/Assets/Scripts/treeFade.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class treeFade : MonoBehaviour {
+
+	public Color fade;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			GetComponent<Renderer> ().material.color = fade;
+
+		}
+	
+	}
+}
+
