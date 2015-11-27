89a9c6a0ad01304729c6306d81302ff37d3089ab
ahmedrage <ahmedrage@hotmail.com>
2015-11-28 02:36:26 +1030

    Added randomized tiles

---
+++ Project Fog/Assets/Scripts/Gm.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gm : MonoBehaviour {
+	public int playerHealth;
+	public int powerUp;
+	public bool permaUp1;
+	public bool permaUp2;
+	public bool permaUp3;
+	public bool permaUp4;
+	public bool permaUp5;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+		if (Input.GetKeyDown("r")) {
+			print ("Reloaded");
+			Application.LoadLevel(Application.loadedLevel);
+		
+		}
+	}
+}
---
+++ Project Fog/Assets/Scripts/gridSquareScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gridSquareScript : MonoBehaviour {
+
+	private int randomEvent;
+	public Sprite event1;
+	public Sprite event2;
+	public Sprite event3;
+	public SpriteRenderer mySpriteRenderer;
+
+	void Start () {
+		mySpriteRenderer = gameObject.GetComponent<SpriteRenderer> ();;
+		randomEvent = Random.Range (1, 3);
+
+		switch (randomEvent) {
+		case 1:
+			mySpriteRenderer.sprite = event1;
+			break;
+		case 2:
+			mySpriteRenderer.sprite = event2;
+			break;
+		case 3:
+			mySpriteRenderer.sprite = event3;
+			break;
+		
+		default:
+			break;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
